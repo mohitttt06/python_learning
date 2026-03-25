f0fa2331ef8915768fbaa8ad9d3b07bad8bbc651
mohitttt06 <mummyandmom@gmail.com>
2026-03-25 20:05:18 +0530

    Add files via upload

---
+++ 5simple_calculator.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+op = input("Enter the operator: ")
+if(op=='+'):
+    print(num1+num2)
+elif(op=='-'):
+    print(num1-num2)
+elif(op=='/'):
+    print(num1/num2)
+elif(op=='%'):
+    print(num1%num2)
+elif(op=='*'):
+    print(num1*num2)
+else:
+    print("Enter the these symbols only +,-,/,%,*")
