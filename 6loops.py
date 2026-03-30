bae3538141fce9d0d589d570b844ab74ce411b05
mohitttt06 <mummyandmom@gmail.com>
2026-03-30 06:34:19 +0530

    day-6 had a gap but today completed loops concept

---
+++ 6loops.py
@@ -0,0 +1,20 @@
+#there are two loops in python one is for loop and other is while loop.
+#we use for loop when we know how many times we need to iterate.
+#we while loop when we dont know how many times to iterate but have a condition with us
+
+
+
+
+for i in range (6):      
+    print("mahishmati")  #here i is declared and initialized here itself.
+                         #and i is also incremented from 0-5 which sums to total 6
+
+
+
+i =0
+while i<5:               #here we initialized i to 0
+                         #then gave a condition i<5
+                         #then also incremented i ourself i=i+1
+                         #so we can conclude that while loop using is same in python as in other languages.
+    print("dude")
+    i=i+1
